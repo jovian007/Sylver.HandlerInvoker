5546e03a8425908505c3395cdc80ea99b1d866bd
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2019-07-16 09:04:03 +0200

    Add docs to models

--- HandlerInvoker.Core/Internal/Transformers/ParameterTransformerCacheEntry.cs
+++ HandlerInvoker.Core/Internal/Transformers/ParameterTransformerCacheEntry.cs
@@ -3,21 +3,44 @@ using System.Reflection;
 
 namespace HandlerInvoker.Core.Internal.Transformers
 {
-    public delegate object TransformerMethod(object source, object destination);
+    public delegate object TransformerFuntion(object source, object destination);
 
     internal class ParameterTransformerCacheEntry
     {
+        /// <summary>
+        /// Gets the source parameter information.
+        /// </summary>
         public TypeInfo Source { get; }
 
+        /// <summary>
+        /// Gets the destination parameter information.
+        /// </summary>
         public TypeInfo Destination { get; }
 
+        /// <summary>
+        /// Gets the method parameter target information type.
+        /// </summary>
         public TypeInfo Target { get; }
 
+        /// <summary>
+        /// Gets the parameter factory to create new instances of the parameter based on his type information.
+        /// </summary>
         public Func<TypeInfo, object> ParameterFactory { get; }
 
-        public TransformerMethod Transformer { get; }
+        /// <summary>
+        /// Gets the parameter transformer function.
+        /// </summary>
+        public TransformerFuntion Transformer { get; }
 
-        public ParameterTransformerCacheEntry(TypeInfo source, TypeInfo destination, TypeInfo target, Func<TypeInfo, object> parameterFactory, TransformerMethod transformer)
+        /// <summary>
+        /// Creates a new <see cref="ParameterTransformerCacheEntry"/> instance.
+        /// </summary>
+        /// <param name="source">Source parameter type information.</param>
+        /// <param name="destination">Destination parameter type information.</param>
+        /// <param name="target">Method target parameter type information.</param>
+        /// <param name="parameterFactory">Parameter factory creator.</param>
+        /// <param name="transformer">Parameter tranformer function.</param>
+        public ParameterTransformerCacheEntry(TypeInfo source, TypeInfo destination, TypeInfo target, Func<TypeInfo, object> parameterFactory, TransformerFuntion transformer)
         {
             this.Source = source;
             this.Destination = destination;
--- HandlerInvoker.Core/Models/TransformerModel.cs
+++ HandlerInvoker.Core/Models/TransformerModel.cs
@@ -5,13 +5,28 @@ namespace HandlerInvoker.Core.Models
 {
     internal class TransformerModel
     {
+        /// <summary>
+        /// Gets the source parameter information to be transformed.
+        /// </summary>
         public TypeInfo Source { get; }
 
+        /// <summary>
+        /// Gets the destiantion parameter informations.
+        /// </summary>
         public TypeInfo Destination { get; }
 
-        public TransformerMethod Transformer { get; }
+        /// <summary>
+        /// Gets the transformation parameter fucntion.
+        /// </summary>
+        public TransformerFuntion Transformer { get; }
 
-        public TransformerModel(TypeInfo source, TypeInfo destination, TransformerMethod transformer)
+        /// <summary>
+        /// Creates a new <see cref="TransformerModel"/> instance.
+        /// </summary>
+        /// <param name="source">Source parameter information.</param>
+        /// <param name="destination">Destination parameter information.</param>
+        /// <param name="transformer">Transformer function.</param>
+        public TransformerModel(TypeInfo source, TypeInfo destination, TransformerFuntion transformer)
         {
             this.Source = source;
             this.Destination = destination;
